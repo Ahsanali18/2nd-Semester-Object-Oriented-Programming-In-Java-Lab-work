e86a9c6663378ad1077a6110c89b5a13d5fddb45
Ahsan-Ali <149705869+Ahsanali18@users.noreply.github.com>
2024-06-02 01:17:25 +0500

    Add files via upload

---
+++ Basic Programs/Addition.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class Addition {
+	
+	public static void main(String[] args) {
+		byte a=5;
+		byte b=2;
+		System.out.println("The initial value of a="+a);
+		System.out.println("The initial value of b="+b);
+		System.out.println("The Sum of a and b="+(a+b));
+	}
+}
+
