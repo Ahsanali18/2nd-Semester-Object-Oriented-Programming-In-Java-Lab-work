9437db559b8c42bad85385e2647b79112a072a58
Ahsan-Ali <149705869+Ahsanali18@users.noreply.github.com>
2024-06-16 11:19:43 +0500

    Add files via upload

---
+++ Arrays/Array_Usage.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class Array_Usage {
+
+	public static void main(String[] args) {
+	/*  int [] marks =new int[10];
+		marks[0]=6;*/
+		
+		int array[]= {1,2,3,4,5,6};
+		for(int element:array)
+		System.out.println(array);
+		
+		
+		
+	}
+
+}
